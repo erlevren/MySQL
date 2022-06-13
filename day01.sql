3b97f49904d0c90e7c83edd0a3edfcdfbe8648d4
Erol Evren <erlevren@gmail.com>
2022-06-13 23:31:23 +0300

    13/06

---
+++ day01.sql
@@ -0,0 +1,39 @@
+create database batch59;
+-- create database: database olustururuz
+
+-- aciklama yazabilmek icin "--" kullanilir
+
+/*
+Java'daki gibi aciklama yapabiliriz 
+*/
+
+
+use batch59; -- birden fazla database var ise "use" komutu ile calistiracagimiz database'i seceriz
+
+
+-- TABLO OLUSTURMA
+create table student
+(
+id varchar(4), 
+name varchar(30),
+age int
+);
+
+-- VERI GIRISI
+insert into student values('1000', 'Ali Can', '35');
+insert into student values('1001', 'Ali Veli', '30');
+insert into student values('1002', 'Erol Evren', '37');
+insert into student values('1000', 'Canan Tan', '29');
+
+
+select * from student; -- student tablosundaki herseyi getirir. * yerine hangi data'yi girersek onu getirir
+
+select id, name from student; -- id ve name sutununu getirir
+
+drop table student; -- student tablosunu siler
+
+
+
+
+
+
---
+++ day01calisma.sql
@@ -0,0 +1,2 @@
+SELECT * FROM deneme.ogrenci where ogr_ad='ali'; -- ali isminde olanlari getirir
+SELECT * FROM deneme.ogrenci where ogr_no=3; -- ogrenci numarasi 3 olanlari getirir
