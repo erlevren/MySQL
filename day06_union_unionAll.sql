c087b146823e85636a81bd9bac28801c7a6d48d3
Erol Evren <erlevren@gmail.com>
2022-06-21 23:31:06 +0300

    21/06

--- day06_union_unionAll.sql
+++ day06_union_unionAll.sql
@@ -47,3 +47,91 @@ select maas, isim from personel where maas>4000
 union
 select maas, sehir from personel where maas>5000;
 
+
+
+/* -----------------------------------------------------------------------------
+  SORU2: Mehmet Ozturk ismindeki kisilerin aldigi maaslari ve Istanbul’daki 
+  personelin maaslarini yüksekten alçaga dogru siralayarak bir tabloda gosteren 
+  sorguyu yaziniz.    
+------------------------------------------------------------------------------*/  
+select maas, isim as isim_sehir
+from personel
+where isim = 'Mehmet Ozturk'
+union
+select maas, sehir
+from personel
+where sehir = 'Istanbul'
+order by maas desc;
+
+
+/* -----------------------------------------------------------------------------
+  SORU3: Honda,Ford ve Tofas’ta calisan  personelin ismini listeleyin
+------------------------------------------------------------------------------*/  
+select isim from personel
+where sirket ='Honda'
+union all
+select isim from personel
+where sirket ='Ford'
+union all
+select isim from personel
+where sirket ='Tofas';
+
+-- tekrarllar gelmesin istersek sadece union yazariz.
+
+select isim, sirket from personel where sirket in ('Honda', 'Tofas', 'Ford');
+
+
+/* -----------------------------------------------------------------------------
+  SORU4: 5000’den az maas alanlarin bilgileri ile, arti Honda calisani olmayanlarin bilgilerini
+ alt alta listeleyen bir sorgu yaziniz. 
+------------------------------------------------------------------------------*/ 
+select isim, maas, sirket from personel where maas<5000
+union
+select isim, maas, sirket from personel where sirket != 'Honda';
+
+
+/* -----------------------------------------------------------------------------
+  SORU5: Ismi Mehmet Ozturk olanlarin sehirlerini + Istanbul’da calismayanlarin isimlerini ve 
+  sehirlerini listeleyen sorguyu yaziniz.
+------------------------------------------------------------------------------*/
+select isim, sehir from personel where isim ='Mehmet Ozturk'
+union
+select isim, sehir from personel where sehir <> 'Istanbul';
+
+
+/*======================== FARKLI TABLOLARDAN BIRLESTIRME ====================*/   
+    
+CREATE TABLE personel_bilgi 
+(
+	id int, 
+	tel char(10) UNIQUE , 
+	cocuk_sayisi int
+); 
+   
+INSERT INTO personel_bilgi VALUES(123, '5302345678' , 5);
+INSERT INTO personel_bilgi VALUES(234, '5422345678', 4);
+INSERT INTO personel_bilgi VALUES(345, '5354561245', 3); 
+INSERT INTO personel_bilgi VALUES(478, '5411452659', 3);
+INSERT INTO personel_bilgi VALUES(789, '5551253698', 2);
+INSERT INTO personel_bilgi VALUES(344, '5524578574', 2);
+INSERT INTO personel_bilgi VALUES(125, '5537488585', 1);
+
+select * from personel_bilgi;
+
+/* -----------------------------------------------------------------------------
+  SORU6: id’si 123456789 olan personelin Personel tablosundan sehir ve 
+  maasini, personel_bilgi tablosundan da (id ilk 3 hanesiyle kaydolmuş=123)
+  tel ve cocuk sayisini yazdirin  
+------------------------------------------------------------------------------*/    
+select sehir as sehir_telefon, maas as maas_cocuksayisi from personel where id = 123456789
+union
+select tel, cocuk_sayisi from personel_bilgi where id=123;
+
+
+/* -----------------------------------------------------------------------------
+  SORU7: Personel tablosundan Istanbul veya Ankara’da calisanlarin id’lerini
+ ve Personel_bilgi tablosundan 2 veya 3 cocugu olanlarin id lerini sorgulayiniz.
+------------------------------------------------------------------------------*/
+select id from personel where sehir in('Istanbul', 'Ankara')
+union
+select id from personel_bilgi where cocuk_sayisi in(2,3);
